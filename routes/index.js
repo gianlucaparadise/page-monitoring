b6086cd85d31ba83005ddce2392f5f2e2e7d14a0
g.paradiso <g.paradiso@accenture.com>
2017-01-21 19:27:43 +0100

    fix

--- routes/index.js
+++ routes/index.js
@@ -21,13 +21,13 @@ router.get('/register', function (req, res, next) {
     console.log("Received a /monitor command from", msg.from.username);
     var url = msg.args();
 
-    pinger = setInterval(function () {
+    /*pinger = setInterval(function () {
       request(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
           reply.text(url); // Show the HTML for the Google homepage.
         }
       });
-    }, 5000);
+    }, 5000);*/
   });
 
   bot.command("stop", function (msg, reply, next) {
