a5794044051cc9370532a90342d531a2cc55d0f1
g.paradiso <g.paradiso@accenture.com>
2017-01-21 17:46:15 +0100

    Bot registering

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var botgram = require("botgram");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/pippo', function(req, res, next) {
-  var pippo = {
-    name: "pippo",
-    surname: "pluto"
-  };
-  
-  res.json(pippo);
+router.get('/register', function (req, res, next) {
+  var bot = botgram("251656776:AAHt3XM9u0ngOTU-lJgi-wBzt5dcN_G2WEU");
+
+  bot.command("start", function (msg, reply, next) {
+    console.log("Received a /start command from", msg.from.username);
+  });
+
+  bot.text(function (msg, reply, next) {
+    console.log("Received a text message:", msg.text);
+    reply.text("hello!");
+  });
+
+  res.json("{yo: yo}");
 });
 
 module.exports = router;
